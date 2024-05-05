2a6deea38a2c108c97a610f915b8b71d43c9dbbd
kushal bhargava <kushal.git01@gmail.com>
2024-05-05 17:28:27 +0530

    MUI added, testing with demo data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Data from "./assets/data.json";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container maxWidth="lg">
+        <Typography variant="h4" align="center" style={{ marginTop: "50px" }}>
+          Job Search
+        </Typography>
+        <Grid container spacing={5} style={{ marginTop: "20px" }}>
+          {Data.map((res, i) => (
+            <Grid item xs={12} sm={6} md={4} key={i}>
+              <Card
+                sx={{ maxWidth: 345 }}
+                style={{ padding: "10px", marginBottom: "30px" }}
+              >
+                <CardMedia
+                  sx={{ height: 140 }}
+                  image={res.image}
+                  title="green iguana"
+                  style={{ borderRadius: "5px" }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {res.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {res.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button variant="contained" size="small">
+                    Share
+                  </Button>
+                  <Button variant="contained" size="small">
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
